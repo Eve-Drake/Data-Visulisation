d74058209c0b0a62920e2c0e1884aec8f04502b3
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-09-20 18:37:37 +0100

    Added working chart

--- src/RenderData.js
+++ src/RenderData.js
@@ -1,21 +1,51 @@
 import React from 'react'
 import DeleteDataPoint from './DeleteDataPoint'
 import EditDataPoint from './EditDataPoint'
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+  } from "recharts";
 
 const RenderData = ({dataArray, setDataArray}) => {
+    
   return (
     <div>
         <div>
-            {dataArray.map((data)=>(
-                <div key={data.id}>
-                    <p>{data.data}</p>
-                    <DeleteDataPoint dataArray={dataArray} setDataArray={setDataArray} id={data.id}/>
-                    <EditDataPoint dataArray={dataArray} setDataArray={setDataArray} id={data.id}/>
+            {dataArray.map((dataObj)=>(
+                <div key={dataObj.id}>
+                    <p>{dataObj.data}</p>
+                    <DeleteDataPoint dataArray={dataArray} setDataArray={setDataArray} id={dataObj.id}/>
+                    <EditDataPoint dataArray={dataArray} setDataArray={setDataArray} id={dataObj.id}/>
                 </div>
             ))}
         </div>
         <div>
-
+            <LineChart
+                width={500}
+                height={300}
+                data={dataArray}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5
+                }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="Id" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line
+                        type="monotone"
+                        dataKey="data"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                    />
+            </LineChart>
         </div>
     </div>
   )
